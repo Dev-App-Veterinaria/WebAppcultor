70b2fa0f186c23f6edb90392f5bbd96b5ad15b5d
Pedro Vinícius de Melo Silva <65138009+PedroViniciusMelo@users.noreply.github.com>
2021-04-09 09:00:13 -0300

    Ajustes no layout
    - Adicionado imagem e ícone do Appcultor.
    - Configurado botões do index para possuírem mesmo tamanho.

--- resources/views/principal/index.blade.php
+++ resources/views/principal/index.blade.php
@@ -8,17 +8,11 @@
                     <div class="col-lg-6 wow fadeInUp">
                         <h1 class="furniture_text ">Gerencie seus<br>dados aqui!</h1>
                         <h5 class="text-light">Escolha abaixo a categoria do APP que você deseja gerenciar.</h5>
-                        <a href="/flores">
-                            <button class="btn btn-light">FLORES</button>
-                        </a>
-                        <br><br>
-                        <a href="/artigos">
-                            <button class="btn btn-dark">ARTIGOS</button>
-                        </a>
-                        <br><br>
-                        <a href="/tags">
-                            <button class="btn btn-light">TAGS</button>
-                        </a>
+                        <div style="width: 150px;">
+                            <a href="/flores" class="btn btn-light btn-block">Doenças</a>
+                            <a href="/artigos" class="btn btn-dark btn-block">Artigos</a>
+                            <a href="/tags" class="btn btn-light btn-block">Artigos</a>
+                        </div>
                     </div>
                     <div class="col-lg-6 d-none d-lg-block wow zoomIn">
                         <div class="img-place floating-animate">
